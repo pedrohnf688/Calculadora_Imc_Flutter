f24de025e8c1fcdc8dd369954517b254fae9e0ae
pedrohnf <pedrohnf2014@gmail.com>
2020-02-10 20:31:31 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController peso = TextEditingController();
+  TextEditingController altura = TextEditingController();
+  String _infoTexto = "Informe seus dados";
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetarCampos() {
+    peso.text = "";
+    altura.text = "";
+    setState(() {
+      _infoTexto = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calcular() {
+    setState(() {
+      double pesoValor = double.parse(peso.text);
+      double alturaValor = double.parse(altura.text);
+      double imc = pesoValor / (alturaValor * alturaValor) / 100;
+
+      if (imc < 18.6) {
+        _infoTexto = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoTexto = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoTexto = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoTexto = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc <= 39.9) {
+        _infoTexto = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoTexto = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.refresh), onPressed: _resetarCampos)
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+                TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso (kg)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: peso,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira seu Peso!";
+                      }
+                      return null;
+                    }),
+                TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (cm)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: altura,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira sua Altura!";
+                      }
+                      return null;
+                    }),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _calcular();
+                        }
+                      },
+                      child: Text("Calcular",
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 25.0)),
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+                Text(_infoTexto,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0))
+              ],
+            ),
+          ),
+        ));
+  }
+}
